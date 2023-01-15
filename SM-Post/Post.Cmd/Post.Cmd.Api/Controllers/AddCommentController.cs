898346fe2660f8da97f06ef72586e36ebcff907c
Reza Kazemi <rezakazemi890@gmail.com>
2023-01-15 23:01:11 +0330

    AddCommentController Added

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/AddCommentController.cs
@@ -0,0 +1,60 @@
+using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Common.DTOs;
+
+namespace Post.Cmd.Api.Controllers;
+
+public class AddCommentController : ControllerBase
+{
+    private readonly ILogger<AddCommentController> _logger;
+    private readonly ICommandDispatcher _commandDispatcher;
+
+    public AddCommentController(ILogger<AddCommentController> logger, ICommandDispatcher commandDispatcher)
+    {
+        _logger = logger;
+        _commandDispatcher = commandDispatcher;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> AddCommentAsync(Guid id, AddCommentCommand command)
+    {
+        try
+        {
+            command.Id = id;
+            await _commandDispatcher.SendAsync(command);
+
+            return Ok(new BaseResponse
+            {
+                Message = "Add Comment  Request Compeleted Successfully"
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            _logger.Log(LogLevel.Warning, ex, "Client made a bad request!");
+            return BadRequest(new BaseResponse
+            {
+                Message = ex.Message
+            });
+        }
+        catch (AggregateNotFoundException ex)
+        {
+            _logger.Log(LogLevel.Warning, ex, "Could not retrive aggregate");
+            return BadRequest(new BaseResponse
+            {
+                Message = ex.Message
+            });
+        }
+        catch (Exception ex)
+        {
+            const string SAFE_ERROR_MESSAGE = "Error while processing request add comment to post!";
+            _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+            return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+            {
+                Message = SAFE_ERROR_MESSAGE
+            });
+        }
+    }
+
+}
