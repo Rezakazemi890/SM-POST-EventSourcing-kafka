1ff7df63591855e4dc699c72d34c40a9499fd4fe
Reza Kazemi <rezakazemi890@gmail.com>
2023-02-05 13:25:32 +0330

    Add dispatcher and handler to queryapi services

--- SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,9 +1,13 @@
 using Confluent.Kafka;
 using CQRS.Core.Consumers;
+using CQRS.Core.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Post.Query.Api.Queries;
+using Post.Query.Domain.Entities;
 using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Dispatchers;
 using Post.Query.Infrastructure.Handlers;
 using Post.Query.Infrastructure.Repositories;
 
@@ -21,9 +25,23 @@ dataContext.Database.EnsureCreated();
 
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IQueryHandler, QueryHandler>();
 builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>();
 builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection(nameof(ConsumerConfig)));
 builder.Services.AddScoped<IEventConsumer, EventConsumer>();
+
+//Register QueryHandler Methods
+
+var queryHandler = builder.Services.BuildServiceProvider().GetRequiredService<IQueryHandler>();
+var dispatcher = new QueryDispatcher();
+
+dispatcher.RegisterHandler<FindAllPostQuery>(queryHandler.HandleAsync);
+dispatcher.RegisterHandler<FindPostByIdQuery>(queryHandler.HandleAsync);
+dispatcher.RegisterHandler<FindPostWithAuthorQuery>(queryHandler.HandleAsync);
+dispatcher.RegisterHandler<FindPostWithCommentsQuery>(queryHandler.HandleAsync);
+dispatcher.RegisterHandler<FindPostWithLikesQuery>(queryHandler.HandleAsync);
+
+builder.Services.AddSingleton<IQueryDispatcher<PostEntity>>(_ => dispatcher);
 
 builder.Services.AddControllers();
 
